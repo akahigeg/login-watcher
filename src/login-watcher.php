47ebcb5ca151d81c34da4c98a48376f5b45a1afe
akahigeg <akahigeg@gmail.com>
2018-06-19 16:22:41 +0900

    add login history menu.

--- src/login-watcher.php
+++ src/login-watcher.php
@@ -11,15 +11,13 @@ License: GPLv2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
-// TODO: アクティベート時のlogin_watcher_histories テーブル作成
-// TODO: ログイン履歴の記録
-// TODO: ログイン履歴を管理画面で一覧表示
-// TODO: アンインストール時のテーブル削除
-
 require_once(plugin_dir_path(__FILE__) . 'constants.php');
 add_option('login_watcher_table_name', LOGIN_WATCHER_TABLE_NAME);
 
 class LoginWatcher {
+  /*
+   * create table on activate
+   */
   public static function activate() {
     global $wpdb;
 
@@ -38,5 +36,28 @@ class LoginWatcher {
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
     dbDelta($sql);
   }
+
+  /*
+   * create table on activate
+   */
+  // TODO: ログイン履歴の記録
+  public static function saveLoginHistory($user_login, $current_user) {
+
+  }
+
+  /*
+   * create table on activate
+   */
+  // TODO: ログイン履歴を管理画面で一覧表示
+  public static function showLoginHistory() {
+    echo "ok";
+  }
+
+  public static function showLoginHistoryMenu() {
+    add_menu_page('ログイン履歴', 'ログイン履歴', 'manage_options', 'login_watcher_login_history', 'LoginWatcher::showLoginHistory', 'dashicons-welcome-learn-more', 3);
+  }
 }
-register_activation_hook( __FILE__, 'LoginWatcher::activate' );
+
+register_activation_hook( __FILE__, 'LoginWatcher::activate');
+add_action('wp_login', 'LoginWatcher::saveHistory', 10, 2);
+add_action('admin_menu', 'LoginWatcher::showLoginHistoryMenu');
