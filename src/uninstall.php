d91fab9619309329153ff09398d9a5971f870e12
akahigeg <akahigeg@gmail.com>
2018-06-21 14:23:12 +0900

    Fix uninstall process.

--- src/uninstall.php
+++ src/uninstall.php
@@ -4,6 +4,7 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 }
 
 // Drop table.
+require_once(plugin_dir_path( __FILE__ ) . 'index.php');
 global $wpdb;
 $table_name = $wpdb->prefix . SimpleLoginHistory::SIMPLE_LOGIN_HISTORY_TABLE_NAME;
-$wpdb->query("DROP TABLE " . $table_name . ";");
+$wpdb->query("DROP TABLE IF EXISTS " . $table_name . ";");
